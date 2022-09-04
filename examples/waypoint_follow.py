48f547ec6b9b87ec6a6c1803e38a21e22d7d6dcd
Tomas Nagy <nagytom4@fel.cvut.cz>
2022-09-04 17:21:03 -0400

    Fix example to show all the changes (MB model, initial state, drive/steer options)

--- examples/waypoint_follow.py
+++ examples/waypoint_follow.py
@@ -11,6 +11,8 @@ from pyglet.gl import GL_POINTS
 """
 Planner Helpers
 """
+
+
 @njit(fastmath=False, cache=True)
 def nearest_point_on_trajectory(point, trajectory):
     """
@@ -25,25 +27,26 @@ def nearest_point_on_trajectory(point, trajectory):
     trajectory: Nx2 matrix of (x,y) trajectory waypoints
         - these must be unique. If they are not unique, a divide by 0 error will destroy the world
     """
-    diffs = trajectory[1:,:] - trajectory[:-1,:]
-    l2s   = diffs[:,0]**2 + diffs[:,1]**2
+    diffs = trajectory[1:, :] - trajectory[:-1, :]
+    l2s = diffs[:, 0] ** 2 + diffs[:, 1] ** 2
     # this is equivalent to the elementwise dot product
     # dots = np.sum((point - trajectory[:-1,:]) * diffs[:,:], axis=1)
-    dots = np.empty((trajectory.shape[0]-1, ))
+    dots = np.empty((trajectory.shape[0] - 1,))
     for i in range(dots.shape[0]):
         dots[i] = np.dot((point - trajectory[i, :]), diffs[i, :])
     t = dots / l2s
-    t[t<0.0] = 0.0
-    t[t>1.0] = 1.0
+    t[t < 0.0] = 0.0
+    t[t > 1.0] = 1.0
     # t = np.clip(dots / l2s, 0.0, 1.0)
-    projections = trajectory[:-1,:] + (t*diffs.T).T
+    projections = trajectory[:-1, :] + (t * diffs.T).T
     # dists = np.linalg.norm(point - projections, axis=1)
     dists = np.empty((projections.shape[0],))
     for i in range(dists.shape[0]):
         temp = point - projections[i]
-        dists[i] = np.sqrt(np.sum(temp*temp))
+        dists[i] = np.sqrt(np.sum(temp * temp))
     min_dist_segment = np.argmin(dists)
     return projections[min_dist_segment], dists[min_dist_segment], t[min_dist_segment], min_dist_segment
+
 
 @njit(fastmath=False, cache=True)
 def first_point_on_trajectory_intersecting_circle(point, radius, trajectory, t=0.0, wrap=False):
@@ -60,15 +63,15 @@ def first_point_on_trajectory_intersecting_circle(point, radius, trajectory, t=0
     first_i = None
     first_p = None
     trajectory = np.ascontiguousarray(trajectory)
-    for i in range(start_i, trajectory.shape[0]-1):
-        start = trajectory[i,:]
-        end = trajectory[i+1,:]+1e-6
+    for i in range(start_i, trajectory.shape[0] - 1):
+        start = trajectory[i, :]
+        end = trajectory[i + 1, :] + 1e-6
         V = np.ascontiguousarray(end - start)
 
-        a = np.dot(V,V)
-        b = 2.0*np.dot(V, start - point)
-        c = np.dot(start, start) + np.dot(point,point) - 2.0*np.dot(start, point) - radius*radius
-        discriminant = b*b-4*a*c
+        a = np.dot(V, V)
+        b = 2.0 * np.dot(V, start - point)
+        c = np.dot(start, start) + np.dot(point, point) - 2.0 * np.dot(start, point) - radius * radius
+        discriminant = b * b - 4 * a * c
 
         if discriminant < 0:
             continue
@@ -76,8 +79,8 @@ def first_point_on_trajectory_intersecting_circle(point, radius, trajectory, t=0
         # else:
         # if discriminant >= 0.0:
         discriminant = np.sqrt(discriminant)
-        t1 = (-b - discriminant) / (2.0*a)
-        t2 = (-b + discriminant) / (2.0*a)
+        t1 = (-b - discriminant) / (2.0 * a)
+        t2 = (-b + discriminant) / (2.0 * a)
         if i == start_i:
             if t1 >= 0.0 and t1 <= 1.0 and t1 >= start_t:
                 first_t = t1
@@ -102,20 +105,20 @@ def first_point_on_trajectory_intersecting_circle(point, radius, trajectory, t=0
     # wrap around to the beginning of the trajectory if no intersection is found1
     if wrap and first_p is None:
         for i in range(-1, start_i):
-            start = trajectory[i % trajectory.shape[0],:]
-            end = trajectory[(i+1) % trajectory.shape[0],:]+1e-6
+            start = trajectory[i % trajectory.shape[0], :]
+            end = trajectory[(i + 1) % trajectory.shape[0], :] + 1e-6
             V = end - start
 
-            a = np.dot(V,V)
-            b = 2.0*np.dot(V, start - point)
-            c = np.dot(start, start) + np.dot(point,point) - 2.0*np.dot(start, point) - radius*radius
-            discriminant = b*b-4*a*c
+            a = np.dot(V, V)
+            b = 2.0 * np.dot(V, start - point)
+            c = np.dot(start, start) + np.dot(point, point) - 2.0 * np.dot(start, point) - radius * radius
+            discriminant = b * b - 4 * a * c
 
             if discriminant < 0:
                 continue
             discriminant = np.sqrt(discriminant)
-            t1 = (-b - discriminant) / (2.0*a)
-            t2 = (-b + discriminant) / (2.0*a)
+            t1 = (-b - discriminant) / (2.0 * a)
+            t2 = (-b + discriminant) / (2.0 * a)
             if t1 >= 0.0 and t1 <= 1.0:
                 first_t = t1
                 first_i = i
@@ -129,23 +132,26 @@ def first_point_on_trajectory_intersecting_circle(point, radius, trajectory, t=0
 
     return first_p, first_i, first_t
 
+
 @njit(fastmath=False, cache=True)
 def get_actuation(pose_theta, lookahead_point, position, lookahead_distance, wheelbase):
     """
     Returns actuation
     """
-    waypoint_y = np.dot(np.array([np.sin(-pose_theta), np.cos(-pose_theta)]), lookahead_point[0:2]-position)
+    waypoint_y = np.dot(np.array([np.sin(-pose_theta), np.cos(-pose_theta)]), lookahead_point[0:2] - position)
     speed = lookahead_point[2]
     if np.abs(waypoint_y) < 1e-6:
         return speed, 0.
-    radius = 1/(2.0*waypoint_y/lookahead_distance**2)
-    steering_angle = np.arctan(wheelbase/radius)
+    radius = 1 / (2.0 * waypoint_y / lookahead_distance ** 2)
+    steering_angle = np.arctan(wheelbase / radius)
     return speed, steering_angle
+
 
 class PurePursuitPlanner:
     """
     Example Planner
     """
+
     def __init__(self, conf, wb):
         self.wheelbase = wb
         self.conf = conf
@@ -165,11 +171,11 @@ class PurePursuitPlanner:
         update waypoints being drawn by EnvRenderer
         """
 
-        #points = self.waypoints
+        # points = self.waypoints
 
         points = np.vstack((self.waypoints[:, self.conf.wpt_xind], self.waypoints[:, self.conf.wpt_yind])).T
-        
-        scaled_points = 50.*points
+
+        scaled_points = 50. * points
 
         for i in range(points.shape[0]):
             if len(self.drawn_waypoints) < points.shape[0]:
@@ -178,7 +184,7 @@ class PurePursuitPlanner:
                 self.drawn_waypoints.append(b)
             else:
                 self.drawn_waypoints[i].vertices = [scaled_points[i, 0], scaled_points[i, 1], 0.]
-        
+
     def _get_current_waypoint(self, waypoints, lookahead_distance, position, theta):
         """
         gets the current waypoint to follow
@@ -186,10 +192,11 @@ class PurePursuitPlanner:
         wpts = np.vstack((self.waypoints[:, self.conf.wpt_xind], self.waypoints[:, self.conf.wpt_yind])).T
         nearest_point, nearest_dist, t, i = nearest_point_on_trajectory(position, wpts)
         if nearest_dist < lookahead_distance:
-            lookahead_point, i2, t2 = first_point_on_trajectory_intersecting_circle(position, lookahead_distance, wpts, i+t, wrap=True)
+            lookahead_point, i2, t2 = first_point_on_trajectory_intersecting_circle(position, lookahead_distance, wpts,
+                                                                                    i + t, wrap=True)
             if i2 == None:
                 return None
-            current_waypoint = np.empty((3, ))
+            current_waypoint = np.empty((3,))
             # x, y
             current_waypoint[0:2] = wpts[i2, :]
             # speed
@@ -215,18 +222,19 @@ class PurePursuitPlanner:
 
         return speed, steering_angle
 
+
 def main():
     """
     main entry point
     """
 
-    work = {'mass': 3.463388126201571, 'lf': 0.15597534362552312, 'tlad': 0.82461887897713965, 'vgain': 0.90338203837889}
-    
+    work = {'mass': 1225.88, 'lf': 0.80597534362552312, 'tlad': 9.6461887897713965, 'vgain': 0.950338203837889}
+
     with open('config_example_map.yaml') as file:
         conf_dict = yaml.load(file, Loader=yaml.FullLoader)
     conf = Namespace(**conf_dict)
 
-    planner = PurePursuitPlanner(conf, 0.17145+0.15875)
+    planner = PurePursuitPlanner(conf, 0.805975 + 1.50876)
 
     def render_callback(env_renderer):
         # custom extra drawing function
@@ -246,22 +254,36 @@ def main():
 
         planner.render_waypoints(env_renderer)
 
-    env = gym.make('f110_gym:f110-v0', map=conf.map_path, map_ext=conf.map_ext, num_agents=1)
+    env = gym.make('f110_gym:f110-v0', map=conf.map_path, map_ext=conf.map_ext,
+                   num_agents=1, timestep=0.001, model='MB', drive_control_mode='vel',
+                   steering_control_mode='angle')
     env.add_render_callback(render_callback)
-    
-    obs, step_reward, done, info = env.reset(np.array([[conf.sx, conf.sy, conf.stheta]]))
+
+    # init vector = [x,y,yaw,steering angle, velocity, yaw_rate, beta]
+    obs, step_reward, done, info = env.reset(
+        np.array([[conf.sx, conf.sy, conf.stheta, 0.0, 5.0, 0.0, 0.0]]))
     env.render()
 
     laptime = 0.0
     start = time.time()
 
+    control_step = 20.0  # ms
+    num_of_sim_steps = int(control_step / (env.timestep * 1000.0))
+
     while not done:
-        speed, steer = planner.plan(obs['poses_x'][0], obs['poses_y'][0], obs['poses_theta'][0], work['tlad'], work['vgain'])
-        obs, step_reward, done, info = env.step(np.array([[steer, speed]]))
+        speed, steer = planner.plan(obs['poses_x'][0], obs['poses_y'][0], obs['poses_theta'][0], work['tlad'],
+                                    work['vgain'])
+
+        step_reward = 0.0
+        for i in range(num_of_sim_steps):
+            obs, rew, done, info = env.step(np.array([[steer, speed]]))
+            step_reward += rew
         laptime += step_reward
-        env.render(mode='human')
-        
-    print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time()-start)
+        env.render(
+            mode='human_fast')  # Naive implementation of 'human' render mode does not work well, use 'human_fast'
+
+    print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time() - start)
+
 
 if __name__ == '__main__':
     main()
